b372de0d4e7b0e0777f34d8fd90dbc5afb1a202c
moura1001 <gmouraneto01@gmail.com>
2023-10-21 17:50:17 -0300

    feat: implementação dos casos de uso da aplicação

---
+++ go-codepix/service/usecase/transaction.go
@@ -0,0 +1,101 @@
+package usecase
+
+import (
+	"fmt"
+
+	"github.com/moura1001/codepix/domain/model"
+)
+
+type TransactionUseCase struct {
+	transactionRepository model.ITransactionRepository
+	pixRepository         model.IPixKeyRepository
+}
+
+func (t *TransactionUseCase) Register(accountId string, amount float64, pixKeyFrom, pixKeyFromKind, description string) (*model.Transaction, error) {
+	errMsgTemplate := "error to register transaction in repository. Details: '%s'"
+
+	account, err := t.pixRepository.FindAccountById(accountId)
+	if err != nil {
+		return nil, fmt.Errorf(errMsgTemplate, err)
+	}
+
+	pixKey, err := t.pixRepository.FindKeyByKind(pixKeyFrom, pixKeyFromKind)
+	if err != nil {
+		return nil, fmt.Errorf(errMsgTemplate, err)
+	}
+
+	transaction, err := model.NewTransaction(account, amount, pixKey, description)
+	if err != nil {
+		return nil, fmt.Errorf(errMsgTemplate, err)
+	}
+
+	err = t.transactionRepository.Save(transaction)
+	if err != nil {
+		return nil, fmt.Errorf(errMsgTemplate, err)
+	}
+
+	return transaction, nil
+}
+
+func (t *TransactionUseCase) Confirm(transactionId string) (*model.Transaction, error) {
+	errMsgTemplate := "error to confirm transaction. Details: '%s'"
+
+	transaction, err := t.transactionRepository.FindById(transactionId)
+	if err != nil {
+		return nil, fmt.Errorf(errMsgTemplate, err)
+	}
+
+	err = transaction.Confirm()
+	if err != nil {
+		return nil, fmt.Errorf(errMsgTemplate, err)
+	}
+
+	err = t.transactionRepository.Save(transaction)
+	if err != nil {
+		return nil, fmt.Errorf(errMsgTemplate, err)
+	}
+
+	return transaction, nil
+}
+
+func (t *TransactionUseCase) Complete(transactionId string) (*model.Transaction, error) {
+	errMsgTemplate := "error to complete transaction. Details: '%s'"
+
+	transaction, err := t.transactionRepository.FindById(transactionId)
+	if err != nil {
+		return nil, fmt.Errorf(errMsgTemplate, err)
+	}
+
+	err = transaction.Complete()
+	if err != nil {
+		return nil, fmt.Errorf(errMsgTemplate, err)
+	}
+
+	err = t.transactionRepository.Save(transaction)
+	if err != nil {
+		return nil, fmt.Errorf(errMsgTemplate, err)
+	}
+
+	return transaction, nil
+}
+
+func (t *TransactionUseCase) Cancel(transactionId string, reason string) (*model.Transaction, error) {
+	errMsgTemplate := "error to cancel transaction. Details: '%s'"
+
+	transaction, err := t.transactionRepository.FindById(transactionId)
+	if err != nil {
+		return nil, fmt.Errorf(errMsgTemplate, err)
+	}
+
+	err = transaction.Cancel(reason)
+	if err != nil {
+		return nil, fmt.Errorf(errMsgTemplate, err)
+	}
+
+	err = t.transactionRepository.Save(transaction)
+	if err != nil {
+		return nil, fmt.Errorf(errMsgTemplate, err)
+	}
+
+	return transaction, nil
+}

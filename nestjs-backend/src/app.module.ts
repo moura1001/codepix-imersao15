ca521eafd6be21b958835beef6e498c9c670e89c
moura1001 <gmouraneto01@gmail.com>
2023-10-23 13:54:55 -0300

    feat: nestjs backend - conexão com o postgres, utilização do typeorm e implementação do módulo relacionado as contas bancárias

--- nestjs-backend/src/app.module.ts
+++ nestjs-backend/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BankAccountsModule } from './bank-accounts/bank-accounts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BankAccount } from './bank-accounts/entities/bank-account.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: (process.env.ENV_MODE === 'dev' || process.env.ENV_MODE === 'test') ? process.env.DSN_TEST : process.env.DSN,
+      entities: [BankAccount],
+      synchronize: /true/i.test(process.env.AUTO_MIGRATE_DB),
+    }),
+    BankAccountsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ nestjs-backend/src/bank-accounts/entities/bank-account.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn, CreateDateColumn } from 'typeorm';
+
+@Entity({ name: 'bank_accounts' })
+export class BankAccount {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    account_number: string;
+
+    @Column()
+    owner_name: string;
+
+    @Column({default:0})
+    balance: number;
+
+    @CreateDateColumn({type:'timestamp'})
+    created_at: Date;
+}
